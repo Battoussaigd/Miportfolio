89dfe0d506e16390c84baf9908e206e8b1489dc1
Battoussaigd <196601442+Battoussaigd@users.noreply.github.com>
2026-06-15 00:38:30 -0400

    Add files via upload

--- src/components/CoursesSection.tsx
+++ src/components/CoursesSection.tsx
@@ -107,9 +107,6 @@ export const CoursesSection = ({ isDark }: CoursesSectionProps) => {
       await fetch(SCRIPT_URL, {
         method: 'POST',
         mode: 'no-cors',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-        },
         body,
       });
 
