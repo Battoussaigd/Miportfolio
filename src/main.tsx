1f3989fb31e4bf50e5c749a723c9bd517bf00218
Battoussaigd <196601442+Battoussaigd@users.noreply.github.com>
2026-06-28 04:29:29 -0400

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
 import Admin from './pages/Admin.tsx';
+import CursoEmprendedores from './pages/CursoEmprendedores.tsx';
+import CursoMujeres from './pages/CursoMujeres.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
@@ -11,6 +13,8 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/admin" element={<Admin />} />
+        <Route path="/cursos/emprendedores" element={<CursoEmprendedores />} />
+        <Route path="/cursos/mujeres" element={<CursoMujeres />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
